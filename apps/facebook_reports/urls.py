4108fd5673526b7dad2da5fd5ac0fca5ee1ec77e
Wade Austin <wade.austin@gmail.com>
2015-09-21 15:48:45 -0500

    tweaks to social report

--- apps/facebook_reports/urls.py
+++ apps/facebook_reports/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
-from .views import FacebookPostReportView
+from .views import FacebookPostReportView, FacebookPageReportView
 # Create your app urls here
 
 urlpatterns = [
     # Example
     url(r'^post-report/$',
         FacebookPostReportView.as_view(),
-        name='facebook_post_report')
+        name='facebook_post_report'),
+
+    url(r'^page-report/$',
+        FacebookPageReportView.as_view(),
+        name='facebook_page_report')
 ]
--- apps/facebook_reports/views.py
+++ apps/facebook_reports/views.py
@@ -7,7 +7,7 @@ from .report_builder import FacebookReportBuilder
 
 
 class FacebookPostReportView(View):
-    template_name = 'facebook_reports/facebook_report_form.html'
+    template_name = 'facebook_reports/facebook_post_report_form.html'
     report_template_name = 'facebook_reports/report_templates/post_report.html'
 
     def get(self, request, *args, **kwargs):
@@ -22,8 +22,6 @@ class FacebookPostReportView(View):
             builder = FacebookReportBuilder()
             report_data = builder.build_post_level_report(form.cleaned_data['data_file'])
 
-            print report_data['end_date']
-
             return render(request, self.report_template_name,
                           {'client': form.cleaned_data['client'],
                            'start_date': report_data['start_date'],
@@ -33,3 +31,34 @@ class FacebookPostReportView(View):
         else:
             return render(request, self.template_name,
                           {'form': form})
+
+
+class FacebookPageReportView(View):
+    template_name = 'facebook_reports/facebook_page_report_form.html'
+    report_template_name = 'facebook_reports/report_templates/page_report.html'
+
+    def get(self, request, *args, **kwargs):
+        form = DataFileUploadForm()
+        return render(request, self.template_name,
+                      {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = DataFileUploadForm(self.request.POST, self.request.FILES)
+
+        if form.is_valid():
+            builder = FacebookReportBuilder()
+            report_data = builder.build_page_level_report(form.cleaned_data['data_file'])
+            return render(request, self.report_template_name,
+                          {'client': form.cleaned_data['client'],
+                           'start_date': report_data['start_date'],
+                           'end_date': report_data['end_date'],
+                           'chart_lables': report_data['chart_labels'],
+                           'reach': report_data['reach'],
+                           'impressions': report_data['impressions'],
+                           'engaged_users': report_data['engaged_users'],
+                           'likes': report_data['likes']})
+        else:
+            return render(request, self.template_name,
+                          {'form': form})
+
+
